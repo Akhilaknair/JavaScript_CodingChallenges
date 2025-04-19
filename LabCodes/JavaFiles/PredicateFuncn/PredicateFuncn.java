2fe2c63aa4c7ccba011f5c27ce035f0d0f5347d6
Akhilaknair <akhilaknair24@gmail.com>
2025-04-20 00:23:34 +0530

    chaining fun

--- LabCodes/JavaFiles/PredicateFuncn/PredicateFuncn.java
+++ LabCodes/JavaFiles/PredicateFuncn/PredicateFuncn.java
@@ -6,12 +6,22 @@ public class PredicateFuncn{
 
 public static void main(String args[])
 {
-    // PredicateFuncn p=new PredicateFuncn();
      Predicate<String>checkLen=s->s.length()<8;
      System.out.println("Len is less than 8 "+checkLen.test("HelloWorld"));
 
+     Predicate<String>oddEvenStr=n->(n.length()%2==0);
+     System.out.println("Len of str is Even ? :: "+oddEvenStr.test("java"));
+
+     
      Predicate<Integer>oddEven=n->(n%2==0);
      System.out.println("Number is Even ? :: "+oddEven.test(45));
+
+     //chaining
+     System.out.println("After Chaing result is " +checkLen.and(oddEvenStr).test("javaTutorial"));
+
+     System.out.println("Using OR funcn "+checkLen.or(oddEvenStr).test("JavaTutorial"));
+     System.out.println("Using negate funcn "+checkLen.negate().test("JavaTutorial"));
+
 }
 
 }
