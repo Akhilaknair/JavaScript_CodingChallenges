032a33844ebd8398a5069fe437211a4ccb9bdf01
Akhilaknair <akhilaknair24@gmail.com>
2025-04-20 00:04:45 +0530

    functions

--- LabCodes/JavaFiles/PredicateFuncn/PredicateFuncn.java
+++ LabCodes/JavaFiles/PredicateFuncn/PredicateFuncn.java
@@ -9,6 +9,9 @@ public static void main(String args[])
     // PredicateFuncn p=new PredicateFuncn();
      Predicate<String>checkLen=s->s.length()<8;
      System.out.println("Len is less than 8 "+checkLen.test("HelloWorld"));
+
+     Predicate<Integer>oddEven=n->(n%2==0);
+     System.out.println("Number is Even ? :: "+oddEven.test(45));
 }
 
 }
